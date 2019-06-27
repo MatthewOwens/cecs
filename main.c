5bf8efbe61f254e4c23a25c85eca6b61cd9fe70c
matthew <matthew@owens.tech>
2019-06-27 21:23:04 +0100

    testing gtk
    following along with the
    [tutorial](https://mesonbuild.com/Tutorial.html)

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include<gtk/gtk.h>
 
 int main(int argc, char **argv)
 {
-	printf("hello\n");
-	return 0;
+	GtkWidget *win;
+	gtk_init(&argc, &argv);
+	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(win), "Hello there");
+	g_signal_connect(win, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_widget_show(win);
+	gtk_main();
 }
