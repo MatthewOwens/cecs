bb3462c4070b97cf2c274343873614eb62b243e1
matthew <matthew@owens.tech>
2019-07-02 21:01:41 +0100

    really basic jsmn examples

--- src/main.c
+++ src/main.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
-#include "statemanager.h"
+#include "jsmn.h"
+#include "string.h"
 
-int first_init()
+void print_toks(jsmntok_t *t, const char *str, int max_toks)
 {
-	printf("init 1\n");
-	return 0;
-}
+	if(!t){
+		fprintf(stderr, "%s: token pointer passed is null!", __func__);
+		return;
+	}
 
-int second_init()
-{
-	printf("init 2\n");
-	return 0;
-}
 
-int generic_destroy()
-{
-	printf("kill\n");
-	return 0;
+	for(int i = 0; i < max_toks; ++i){
+		if(t[i].type == JSMN_UNDEFINED)
+			return;
+		printf("%.*s\n", t[i].end - t[i].start, str + t[i].start);
+	}
 }
 
 int main(int argc, char **argv)
 {
-	State state_one = {0};
-	State state_two = {0};
-	StateManager sm;
+	const char str[] = "{ \"name\": \"Jack\", \"age\" : 27 }";
+	const char str2[] = "{ \"object\": { \"int\" : 99, \"bool\":false}}";
+	int tokens = -1;
 
-	StateManager_init(&sm);
+	jsmn_parser p;
+	jsmntok_t t[128];
 
-	state_one.init = first_init;
-	state_one.destroy = generic_destroy;
-	state_two.init = second_init;
+	jsmn_init(&p);
+	tokens = jsmn_parse(&p, str2, strlen(str2), t, 128);
 
-	StateManager_push(&sm,&state_one);
-	StateManager_push(&sm,&state_two);
-	StateManager_pop(&sm);
-	StateManager_pop(&sm);
-
-	printf("hello\n");
+	if(tokens < 0){
+		printf("jsmn error: %d\n", tokens);
+	} else {
+		printf("jsmn parsed %d dokens from %s\n", tokens, str2);
+		print_toks(t, str2, tokens);
+	}
 	return 0;
 }
+
