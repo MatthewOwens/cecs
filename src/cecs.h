c2eb757b1c27dacc3e2a1d7d6b6f14e147b37df6
MatthewOwens <matthew@owens.tech>
2019-08-03 19:08:29 +0100

    added component struct

--- src/cecs.h
+++ src/cecs.h
@@ -2,10 +2,20 @@
 #include <stdint.h>
 #include <stddef.h>
 
-struct cecs_component{
+typedef void (*cecs_sys_func)();
+
+struct cecs_component
+{
 	void *data;
 	size_t size;
 	uint32_t key;
+};
+
+struct cecs_system
+{
+	cecs_sys_func function;
+	uint32_t inclusion_mask;
+	uint32_t exclusion_mask;
 };
 
 enum cecs_state
@@ -20,11 +30,21 @@ struct cecs{
 
 	struct cecs_component *components;
 	int num_components;
+
+	struct cecs_system *systems;
+	int num_systems;
 };
 
 struct cecs* cecs_init();
 int cecs_free(struct cecs* cecs);
-int cecs_start(struct cecs* cecs)
+int cecs_start(struct cecs* cecs);
 
-// Registers data struct with cecs, returns 0 on success
+// registers data struct with cecs, returns 0 on success
 int cecs_reg_component(struct cecs* cecs, void *data, size_t size);
+
+/*
+ * registers a function with cecs, return 0 on success
+ * incl_mask and excl_mask are the relevant component keys bitwise or'd together
+*/
+int cecs_reg_system(struct cecs* cecs, cecs_sys_func func,
+		    uint32_t incl_mask, uint32_t excl_mask);
