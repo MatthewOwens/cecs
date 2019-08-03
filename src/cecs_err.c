99e29dd787fa10e2dfb6c2271c2327985659b0ae
MatthewOwens <matthew@owens.tech>
2019-08-03 15:53:23 +0100

    added coloured output to cecs_err

--- src/cecs_err.c
+++ src/cecs_err.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "cecs_err.h"
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 static enum cecs_errno lasterrno = CECSE_NONE;
 
 static char* cecs_errmsg[] = {
@@ -14,7 +22,9 @@ static char* cecs_errmsg[] = {
 int cecse(enum cecs_errno err)
 {
 	if (err != CECSE_NONE)
-		fprintf(stderr, "cecs error: %s", cecs_errmsg[err]);
+		fprintf(stderr,
+		ANSI_COLOR_RED "cecs error: %s\n" ANSI_COLOR_RESET,
+		cecs_errmsg[err]);
 	return err;
 }
 
