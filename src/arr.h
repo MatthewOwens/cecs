ae51c04e70df8b9f948dae6b50f9083184f33d25
MatthewOwens <matthew@owens.tech>
2019-08-05 21:30:47 +0100

    updated arr so that it doesn't realloc with every push

--- src/arr.h
+++ src/arr.h
@@ -5,31 +5,48 @@
 
 #pragma once
 
-#define array(type)					\
-	struct {					\
-		type* data;				\
-		size_t length;				\
-	}						
+#define array(type)						\
+	struct {						\
+		type* data;					\
+		size_t length;					\
+		size_t capacity;
+	}							
 
-#define array_init(){					\
-	.data = NULL;					\
-	.length = 0;					\
+#define array_init(){						\
+	.data = NULL;						\
+	.length = 0;						\
+	.capacity = 0;						\
 }
 
-#define array_free(array){				\
-	do {						\
-		free(array.data);			\
-		array.data = NULL;			\
-		array.length = 0;			\
-	} while(0)					\
-}
+#define array_free(array)					\
+	do {							\
+		free(array.data);				\
+		array.data = NULL;				\
+		array.length = 0;				\
+		array.capacity = 0;				\
+	} while(0)					
 
-#define array_push(array, element)			\
-  do {							\
-      array.data = realloc(array.data,			\
-                           sizeof(*array.data) *	\
-                             (array.lengthgth + 1));	\
-      array.data[array.length] = element;		\
-      array.length++;					\
-  } while (0)
-
+//#define array_push(array, element)			\
+//  do {							\
+//      array.data = realloc(array.data,			\
+//                           sizeof(*array.data) *	\
+//                             (array.length + 1));	\
+//      array.data[array.length] = element;		\
+//      array.length++;					\
+//  } while (0)
+#define array_push(array, element)				\
+do {								\
+	if (array.capacity == 0) {				\
+		array.data = malloc(sizeof(*array.data));	\
+		array.data[0] = element;			\
+		array.capacity = 1;				\
+		array.length = 1;				\
+	} if (array.length == array.capacity) {			\
+		array.capacity *= 2;				\
+		type* tmp = reallocarray(array.data,		\
+			array.capacity, sizeof(*array.data))	\
+		if (tmp == NULL) return				\
+		array.data = tmp;				\
+		array.data[array.length] = element;		\
+		array.length++;					\
+} while(0)
