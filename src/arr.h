f2e095c53592ad1418861696a0c80fe22b2035f2
MatthewOwens <matthew@owens.tech>
2019-08-09 22:53:18 +0100

    added varray definition & init

--- src/arr.h
+++ src/arr.h
@@ -5,37 +5,54 @@
 
 #pragma once
 
-#define array(type)						\
-	struct {						\
-		type* data;					\
-		size_t length;					\
-		size_t capacity;				\
-	}							
+#define array(type)							\
+	struct {							\
+		type* data;						\
+		size_t length;						\
+		size_t capacity;					\
+	}								
 
-#define array_init()						\
-{								\
-	.data = NULL,						\
-	.length = 0,						\
-	.capacity = 0						\
+#define varray								\
+	struct {							\
+		void* data;						\
+		size_t length;						\
+		size_t capacity;					\
+		size_t size;						\
+	}
+
+#define array_init()							\
+{									\
+	.data = NULL,							\
+	.length = 0,							\
+	.capacity = 0							\
 }
-#define array_free(array)					\
-	do {							\
-		free(array.data);				\
-		array.data = NULL;				\
-		array.length = 0;				\
-		array.capacity = 0;				\
+
+#define varray_init(size)						\
+{									\
+	.data = NULL,							\
+	.length = 0,							\
+	.capacity = 0,							\
+	.size = size							\
+}
+
+#define array_free(array)						\
+	do {								\
+		free(array.data);					\
+		array.data = NULL;					\
+		array.length = 0;					\
+		array.capacity = 0;					\
 	} while(0)					
 
-#define array_push(array, element)				\
-({								\
+#define array_push(array, element)					\
+({									\
 	printf("cap is %ld len is %ld\n", array.capacity, array.length);\
-	if (array.capacity == 0) {				\
-		array.data = malloc(sizeof(*array.data));	\
-		array.data[0] = element;			\
-		array.capacity = 1;				\
-		array.length = 1;				\
-	} else {						\
-		if (array.length == array.capacity) {		\
+	if (array.capacity == 0) {					\
+		array.data = malloc(sizeof(*array.data));		\
+		array.data[0] = element;				\
+		array.capacity = 1;					\
+		array.length = 1;					\
+	} else {							\
+		if (array.length == array.capacity) {			\
 			array.capacity *= 2;				\
 									\
 			void* tmp = reallocarray(array.data,		\
@@ -51,4 +68,5 @@
 		array.length += 1;					\
 	}								\
 })
-//} while (0)
+
+//TODO: varray_push()
