8ee1c81519d1ba571eedae8c059fa79c5cf30898
MatthewOwens <matthew@owens.tech>
2019-08-12 17:03:41 +0100

    fixed array_push() indentation

--- src/arr.h
+++ src/arr.h
@@ -36,37 +36,37 @@
 }
 
 #define array_free(array)\
-	do {\
-		free(array.data);\
-		array.data = NULL;\
-		array.length = 0;\
-		array.capacity = 0;\
-	} while(0)
+do {\
+	free(array.data);\
+	array.data = NULL;\
+	array.length = 0;\
+	array.capacity = 0;\
+} while(0)
 
 #define array_push(array, element)\
 ({\
-printf("cap is %ld len is %ld\n", array.capacity, array.length);\
-if (array.capacity == 0) {\
-array.data = malloc(sizeof(*array.data));\
-array.data[0] = element;\
-array.capacity = 1;\
-array.length = 1;\
-} else {\
-if (array.length == array.capacity) {\
-array.capacity *= 2;\
-\
-void* tmp = reallocarray(array.data,\
-array.capacity, sizeof(*array.data));\
-if (tmp != NULL) {\
-array.data = tmp;\
-} else {\
-fprintf(stderr, "arr.h - realloc failed!\n");\
-return 1;\
-}\
-}\
-array.data[array.length] = element;\
-array.length += 1;\
-}\
+	 printf("cap is %ld len is %ld\n", array.capacity, array.length);\
+	 if (array.capacity == 0) {\
+		 array.data = malloc(sizeof(*array.data));\
+		 array.data[0] = element;\
+		 array.capacity = 1;\
+		 array.length = 1;\
+	 } else {\
+		 if (array.length == array.capacity) {\
+		 array.capacity *= 2;\
+		 \
+		 void* tmp = reallocarray(array.data,\
+			 array.capacity, sizeof(*array.data));\
+			 if (tmp != NULL) {\
+				 array.data = tmp;\
+			 } else {\
+				 fprintf(stderr, "arr.h - realloc failed!\n");\
+				 return 1;\
+			 }\
+		 }\
+		 array.data[array.length] = element;\
+		 array.length += 1;\
+	 }\
 })
 
 //TODO: varray_push()
