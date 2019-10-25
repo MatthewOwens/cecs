a2a7622b1ceb8dc947a4a980f4fce16980f18321
MatthewOwens <matthew@owens.tech>
2019-10-25 12:01:09 +0100

    fixed syntax error

--- src/cecs_entity.c
+++ src/cecs_entity.c
@@ -114,7 +114,7 @@ int cecs_ent_rem_component(struct cecs *cecs, int ent, char* name)
 
 int cecs_ent_get_mask(struct cecs* cecs, int ent, uint32_t* mask)
 {
-	if(cecs == NULL || mask == NULL ent < 0 || ent >= cecs->num_entities)
+	if(cecs == NULL || mask == NULL || ent < 0 || ent >= cecs->num_entities)
 		return cecse(CECSE_INVALID_VALUE);
 
 	return(cecs->entities[ent].mask);
