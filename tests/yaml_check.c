18b4451519fdfa3c48fe0844ce0979c7ec412d7b
MatthewOwens <matthew@owens.tech>
2020-01-08 13:06:43 +0000

    split checks, added fixtures

--- tests/yaml_check.c
+++ tests/yaml_check.c
@@ -24,37 +24,59 @@ typedef struct {
 	const char* str;
 }s_name;
 
-START_TEST(yaml_position)
-{
-	s_position pos = {0};
-	s_velocity vel = {0};
-	s_uv uv = {0};
-	s_name name = {0};
+static s_position pos = {0};
+static s_velocity vel = {0};
+static s_uv uv = {0};
+static s_name name = {0};
 
-	//TODO: call component load func
-	
+void yaml_init()	// TODO: stub
+{
+}
+
+void yaml_teardown()	// TODO: stub
+{
+}
+
+START_TEST(position_loaded)
+{
 	ck_assert_float_eq(pos.x, 5.f);
 	ck_assert_float_eq(pos.y, 5.f);
 	ck_assert_float_eq(pos.z, 5.f);
+}
+END_TEST
 
+START_TEST(velocity_loaded)
+{
 	ck_assert_float_eq(vel.x, 3.0);
 	ck_assert_float_eq(vel.y, 0.0);
 	ck_assert_float_eq(vel.z, 0.0);
+}
+END_TEST
 
+START_TEST(uv_loaded)
+{
 	ck_assert_float_eq(uv.u, 1.0);
 	ck_assert_float_eq(uv.v, 1.0);
+}
+END_TEST
 
-	ck_assert_ptr_null(name.str);
+START_TEST(name_loaded)
+{
+	ck_assert_str_eq(name.str, "test");
 }
 END_TEST
 
 Suite * yaml_suite(void)
 {
 	Suite * s;
-	TCase * tc = tcase_create("yaml init");
+	TCase * load_case = tcase_create("yaml load");
 	s = suite_create("yaml suite");
 
-	tcase_add_test(tc, yaml_position);
-	suite_add_tcase(s, tc);
+	tcase_add_unchecked_fixture(load_case, yaml_init, yaml_teardown);
+	tcase_add_test(load_case, position_loaded);
+	tcase_add_test(load_case, velocity_loaded);
+	tcase_add_test(load_case, uv_loaded);
+	tcase_add_test(load_case, name_loaded);
+	suite_add_tcase(s, load_case);
 	return s;
 }
