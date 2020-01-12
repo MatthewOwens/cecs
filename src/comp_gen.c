06b79ac8535b295b1cd0f3633032ad4e5fe55667
MatthewOwens <matthew@owens.tech>
2020-01-12 12:02:32 +0000

    added set_outfiles()

--- src/comp_gen.c
+++ src/comp_gen.c
@@ -8,6 +8,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+static char* outfiles[2];
 
 void usage(char* exe)
 {
@@ -29,11 +32,31 @@ int argsValid(int argc, char* argv[])
 	return 0;
 }
 
+void set_outfiles(char *arg)
+{
+	int size = strlen(arg);
+	char* end[2] = {".c", ".h"};
+
+	for( int i = 0; i < 2; ++i)
+	{
+		outfiles[i] = malloc(strlen(arg) + 2);
+		strcpy(outfiles[i], arg);
+		strcat(outfiles[i], end[i]);
+	}
+
+	//outfiles[1] = malloc(strlen(arg) + 3);
+	//outfiles[1] = strcat(".c", arg);
+	printf("YEET\n");
+}
+
 int main(int argc, char* argv[])
 {
 	if(argsValid(argc, argv) == 1) {
 		exit(1);
 	}
+
 	printf(("args OK\n"));
+	set_outfiles(argv[2]);
+	printf("outfiles are '%s' and '%s'\n", outfiles[0], outfiles[1]);
 }
 
