7d3c8e52dcb1388e65de6ef4ccb0b80582212fa1
MatthewOwens <matthew@owens.tech>
2020-01-15 11:21:30 +0000

    comp_gen: got basic structure of header file generating

--- src/comp_gen.c
+++ src/comp_gen.c
@@ -9,16 +9,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <yaml.h>
 
 static char* outfiles[2];
+static char* yaml_events[YAML_MAPPING_END_EVENT + 1]=
+{
+    "YAML_NO_EVENT",
+    "YAML_STREAM_START_EVENT",
+    "YAML_STREAM_END_EVENT",
+    "YAML_DOCUMENT_START_EVENT",
+    "YAML_DOCUMENT_END_EVENT",
+    "YAML_ALIAS_EVENT",
+    "YAML_SCALAR_EVENT",
+    "YAML_SEQUENCE_START_EVENT",
+    "YAML_SEQUENCE_END_EVENT",
+    "YAML_MAPPING_START_EVENT",
+    "YAML_MAPPING_END_EVENT"
+};
 
+enum {
+	C=0,
+	H=1
+};
+ 
 void usage(char* exe)
 {
 	printf("%s, usage:\n\t%s <input yml file> <output .h/.c name>\n",
 	exe, exe);
 }
 
-int argsValid(int argc, char* argv[])
+int args_valid(int argc, char* argv[])
 {
 	if(argc != 3) {
 		usage(argv[0]);
@@ -34,7 +54,6 @@ int argsValid(int argc, char* argv[])
 
 void set_outfiles(char *arg)
 {
-	int size = strlen(arg);
 	char* end[2] = {".c", ".h"};
 
 	for( int i = 0; i < 2; ++i)
@@ -45,14 +64,112 @@ void set_outfiles(char *arg)
 	}
 }
 
+const char* determine_type(const char* value)	//TODO: stub
+{
+	return "<TYPE>";
+}
+
+int parse_event(yaml_parser_t *p, yaml_event_t *e, FILE *c, FILE *h)
+{
+
+	/*
+	 * File seems to start off with a mapping event and switch later
+	 * After we've parsed the first event we'll switch this to an empty
+	 * event for the rest of the program
+ 	*/
+	static yaml_event_type_t start_event = YAML_MAPPING_START_EVENT;
+	static yaml_event_t prev_event;
+	static char *key = NULL;
+	static char *value = NULL;
+
+	if(!yaml_parser_parse(p, e)){
+		fprintf(stderr, "error parsing yml!\n");
+		return 0;
+	}
+
+	/* TODO: process the event */
+	printf("event: %s", yaml_events[e->type]);
+	if(e->type == YAML_SCALAR_EVENT){
+		printf("\t| %s\n", e->data.scalar.value);
+	} else if (e->type == YAML_MAPPING_START_EVENT){
+		printf("\t| %s\n", e->data.mapping_start.tag);
+	}
+	printf("\n");
+
+	/*********************
+	 * parsing the event *
+	 ********************/
+
+	// new struct is being defined
+	if(e->type == start_event){
+		// if not at the first struct definition, close the brace
+		// doc ends with 2 end mappings, so we exclude the second
+		if(start_event != YAML_MAPPING_START_EVENT && 
+		prev_event.type != YAML_MAPPING_END_EVENT){
+			fprintf(h, "}\n");
+		}
+	} else if (e->type == YAML_SCALAR_EVENT)
+	{
+		// if we've started a new struct
+		if(prev_event.type == start_event) {
+			fprintf(h, "struct %s_component {\n",
+			e->data.scalar.value);
+			start_event = YAML_MAPPING_END_EVENT;
+		} else {
+			if (key == NULL) {
+				key = e->data.scalar.value;
+			} else {
+				value = e->data.scalar.value;
+				fprintf(h, "\t %s %s = %s;\n",
+					determine_type(value), key, value);
+				key = NULL;
+				value = NULL;
+			}
+		}
+	}
+	if(e->type == YAML_MAPPING_END_EVENT) {
+		// we've hit a new line, clear our values
+		key = NULL;
+		value = NULL;
+	}
+
+	prev_event = *e;
+	return (e->type == YAML_STREAM_END_EVENT);
+}
+
+void build_files(char *in)
+{
+	yaml_parser_t parser;
+	yaml_event_t event;
+	FILE *infile = fopen(in, "rb");
+	FILE *cfile = fopen(outfiles[0], "w+");
+	FILE *hfile = fopen(outfiles[1], "w+");
+	int done = 0;
+
+	yaml_parser_initialize(&parser);
+	yaml_parser_set_input_file(&parser, infile);
+
+	do {
+		done = parse_event(&parser, &event, cfile, hfile);
+	} while(!done);
+
+	yaml_parser_delete(&parser);
+
+	fclose(infile);
+	fclose(cfile);
+	fclose(hfile);
+}
+
 int main(int argc, char* argv[])
 {
-	if(argsValid(argc, argv) == 1) {
+	if(args_valid(argc, argv) == 1) {
 		exit(1);
 	}
 
 	printf(("args OK\n"));
 	set_outfiles(argv[2]);
 	printf("outfiles are '%s' and '%s'\n", outfiles[0], outfiles[1]);
+
+	build_files(argv[1]);
 }
 
