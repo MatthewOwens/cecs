7e93b04ba7f597465eaba4991d209c98a6deef52
MatthewOwens <matthew@owens.tech>
2020-02-02 19:43:53 +0000

    added schemas & load attempt

--- src/entity_load.c
+++ src/entity_load.c
@@ -2,26 +2,57 @@
 #include <cyaml/cyaml.h>
 #include "cecs_err.h"
 
-struct in_ent{
-	char *name;
-	char **components;
+struct entity{
+	const char *name;
+	const char **components;
+	unsigned int components_count;
 };
 
-static struct in_ent ent = {0};
+struct loaded_entities{
+	struct entity* entity;
+	unsigned int entity_count;
+};
+
+struct loaded_entities *in_ents = NULL;
+
+/* default config from examples */
+static const cyaml_config_t config = {
+	.log_level = CYAML_LOG_WARNING, /* Logging errors and warnings only. */
+	.log_fn = cyaml_log,            /* Use the default logging function. */
+	.mem_fn = cyaml_mem,            /* Use the default memory allocator. */
+};
 
 /* Schema for string pointer values (used in sequences of strings). */
-static const cyaml_schema_value_t copmonent_entry = {
+static const cyaml_schema_value_t component_entry = {
 	CYAML_VALUE_STRING(CYAML_FLAG_POINTER, char, 0, CYAML_UNLIMITED),
 };
 
 /* Individual entity mapping */
-//TODO:component sequence correct?
-static const cyaml_schema_field_t top_level_mapping_schema[] = {
-	CYAML_FIELD_STRING_PTR("name", CYAML_FLAG_POINTER, struct in_ent,
+static const cyaml_schema_field_t entity_field_schema[] = {
+	CYAML_FIELD_STRING_PTR("name", CYAML_FLAG_POINTER, struct entity,
 		name, 0, CYAML_UNLIMITED),
-	CYAML_FIELD_SEQUENCE("components", CYAML_FLAG_POINTER, struct in_ent,
-		components, &component_entry, 0, CYAML_UNLIMITED),
+	CYAML_FIELD_SEQUENCE_COUNT("components", CYAML_FLAG_POINTER,
+		struct entity, components, components_count, &component_entry,
+		0, CYAML_UNLIMITED),
 	CYAML_FIELD_END
+};
+
+static const cyaml_schema_value_t entity_schema = {
+	CYAML_VALUE_MAPPING(CYAML_FLAG_POINTER, struct entity,
+		entity_field_schema),
+};
+
+/* Top level entities mapping */
+static const cyaml_schema_field_t entities_field_schema[] = {
+	CYAML_FIELD_SEQUENCE("entities", CYAML_FLAG_POINTER,
+		struct loaded_entities, entity, &entity_schema, 0,
+		CYAML_UNLIMITED),
+	CYAML_FIELD_END
+};
+
+static const cyaml_schema_value_t entities_schema = {
+	CYAML_VALUE_MAPPING(CYAML_FLAG_POINTER, struct loaded_entities,
+		entities_field_schema),
 };
 
 int cecs_load_ent_yaml( struct cecs* cecs, const char* filename)
@@ -29,5 +60,8 @@ int cecs_load_ent_yaml( struct cecs* cecs, const char* filename)
 	if(cecs == NULL) {
 		cecse_msg(CECSE_NULL, __FUNCTION__);
 	}
+
+	cyaml_err_t err = cyaml_load_file(filename, &config, &entities_field_schema,
+			(void **) &in_ents, NULL);
 	return cecse_msg(CECSE_STUB, __FUNCTION__);
 }
