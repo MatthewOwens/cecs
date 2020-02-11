9971ad3954a2f2a9f927cb30ca510cd3fb3b29a3
MatthewOwens <matthew@owens.tech>
2020-02-11 12:43:43 +0000

    [systems-yml] added stub system load functions

---
+++ src/system_load.c
@@ -0,0 +1,7 @@
+#include "system_load.h"
+#include "cecs_err.h"
+
+int cecs_load_sys_yaml( struct cecs* cecs, const char* filename)
+{
+	return cecse(CECSE_STUB);
+}
---
+++ src/system_load.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "cecs.h"
+
+int cecs_load_sys_yaml( struct cecs* cecs, const char* filename);
