0b2d68fcaaa1a03eab31980857ae81f16442d2a8
MatthewOwens <matthew@owens.tech>
2020-02-11 12:44:51 +0000

    [systems-yml] added stub system tests

--- tests/sys_yaml_check.c
+++ tests/sys_yaml_check.c
@@ -6,7 +6,62 @@
 #include "entity_load.h"
 #include "components.h"
 
-START_TEST(stub)
+#define ENTITY_COUNT 5
+
+static struct cecs *cecs = NULL;
+static struct cecs_entity *entities[ENTITY_COUNT];
+
+static void ent_init()
+{
+	cecs_load_ent_yaml(cecs, "entities.yml");
+	cecs_add_entity(cecs, "actor", &entities[0]);
+	cecs_add_entity(cecs, "named_actor", &entities[1]);
+	cecs_add_entity(cecs, "map_location", &entities[2]);
+	cecs_add_entity(cecs, "actor", &entities[3]);
+	cecs_add_entity(cecs, "map_location", &entities[4]);
+}
+
+static void ent_teardown()
+{
+	for(int i = 0; i < ENTITY_COUNT; ++i){
+			cecs_rem_entity(cecs, &entities[i]);
+	}
+}
+
+static void suite_init()
+{
+	cecs = cecs_init();
+	reg_loaded_components(cecs);
+	ent_init();
+}
+
+static void suite_teardown()
+{
+	ent_teardown();
+	cecs_free(cecs);
+}
+
+static void yaml_init()	// TODO: stub
+{
+}
+
+static void yaml_teardown()// TODO: stub
+{
+}
+
+START_TEST(systems_added)
+{
+	ck_abort_msg("STUB!\n");
+}
+END_TEST
+
+START_TEST(movement_load_correct)
+{
+	ck_abort_msg("STUB!\n");
+}
+END_TEST
+
+START_TEST(movement_runs_correct)
 {
 	ck_abort_msg("STUB!\n");
 }
@@ -17,7 +72,12 @@ Suite * sys_yaml_suite(void)
 	Suite * s = suite_create("system yaml suite");
 	TCase * yaml_load = tcase_create("yaml load");
 
-	tcase_add_test(yaml_load, stub);
+	tcase_add_unchecked_fixture(yaml_load, suite_init, suite_teardown);
+	tcase_add_unchecked_fixture(yaml_load, yaml_init, yaml_teardown);
+
+	tcase_add_test(yaml_load, systems_added);
+	tcase_add_test(yaml_load, movement_load_correct);
+	tcase_add_test(yaml_load, movement_runs_correct);
 
 	suite_add_tcase(s, yaml_load);
 	return s;
