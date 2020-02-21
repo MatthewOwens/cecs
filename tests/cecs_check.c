f208c90d188319469443fe07f6c6078f62ebb313
MatthewOwens <matthew@owens.tech>
2020-02-21 07:56:09 +0000

    [systems-registration-rework] removed systems checks from cecs_check
    tests were invalid with new yaml workflow, and it was an inappropriate
    place to have the tests in the fiorst place

--- tests/cecs_check.c
+++ tests/cecs_check.c
@@ -10,25 +10,6 @@
 
 static struct cecs *cecs = NULL;
 
-typedef struct {
-	float x;
-	float y;
-	float z;
-} testPosComponent;
-
-typedef struct {
-	char *str;
-} testStrComponent;
-
-typedef struct {
-	float u;
-	float v;
-} testUVComponent;
-
-static testPosComponent posComp = {0};
-static testStrComponent strComp = {0};
-static testUVComponent uvComp = {0};
-
 void init_setup()
 {
 	cecs = cecs_init();
@@ -38,64 +19,6 @@ void init_teardown()
 {
 	cecs_free(cecs);
 }
-
-int tsys_init(struct cecs* cecs)
-{
-}
-
-int tsys_free(struct cecs* cecs)
-{
-}
-
-void tsys_run()
-{
-	//  getting the system
-	struct cecs_system* sys = cecs_system(cecs, "test-sys");
-
-	testPosComponent* positions = cecs_component(cecs, "position")->data;
-	testUVComponent* uvs = cecs_component(cecs, "uv")->data;
-
-	for(int i = 0; i < cecs->num_entities; ++i){
-		// if i is in free_entities, continue
-		if(cecs->free_entities.length != 0){
-			for(int j = 0; j < cecs->free_entities.length; ++i){
-				if(i == cecs->free_entities.data[i])
-					continue;
-			}
-		}
-
-		// Skipping if the entity contains an excluded component
-		if((cecs->entities[i].mask & sys->exclusion_mask) != 0)
-			continue;
-
-
-		// Actual data transformation
-		positions[i].x += 1.f;
-		positions[i].y += 1.5f;
-		positions[i].z += 2.f;
-
-		uvs[i].u = 5.f;
-		uvs[i].v = 5.f;
-	}
-}
-
-void sys_setup()
-{
-	cecs_reg_system(cecs, "test-sys");
-	cecs_sys_set_incl(cecs, "test-sys",
-			  cecs_component_key(cecs, "position") |
-			  cecs_component_key(cecs, "uv"));
-	cecs_sys_set_excl(cecs, "test-sys",
-			  cecs_component_key(cecs, "string"));
-	cecs_sys_set_funcs(cecs, "test-sys", tsys_init, tsys_run, tsys_free);
-}
-
-void sys_teardown()
-{
-	cecs_rem_system(cecs, "test-sys");
-}
-
-void stub() { ck_abort_msg("STUB - test not implemented\n"); }
 
 START_TEST(cecs_check_init)
 {
@@ -110,123 +33,17 @@ START_TEST(cecs_check_init)
 }
 END_TEST
 
-
-START_TEST(cecs_check_add_sys)
-{
-	struct cecs_system* sys = cecs_system(cecs, "test-sys");
-	ck_assert_int_eq(cecs->num_systems, 1);
-	ck_assert_ptr_nonnull(cecs->systems);
-	ck_assert_int_eq(sys->inclusion_mask,
-		cecs_component_key(cecs,"position") |
-		cecs_component_key(cecs, "uv") );
-
-	ck_assert_uint_eq(sys->exclusion_mask,
-			cecs_component_key(cecs, "string"));
-}
-END_TEST
-
-START_TEST(cecs_check_system_func)
-{
-	ck_assert_ptr_nonnull(cecs_system(cecs, "test-sys"));
-	ck_assert_ptr_null(cecs_system(NULL, "test-sys"));
-	ck_assert_ptr_null(cecs_system(cecs, "not a real system"));
-	ck_assert_ptr_null(cecs_system(NULL, NULL));
-}
-END_TEST
-
-void check_ent_untouched(testPosComponent* pos, testUVComponent* uv)
-{
-	ck_assert_float_eq(pos->x, 0.f);
-	ck_assert_float_eq(pos->y, 0.f);
-	ck_assert_float_eq(pos->z, 0.f);
-	ck_assert_float_eq(uv->u, 0.f);
-	ck_assert_float_eq(uv->v, 0.f);
-}
-
-START_TEST(cecs_check_system_run)
-{
-	struct cecs_system* sys = cecs_system(cecs, "test-sys");
-	testPosComponent* positions = cecs_component(cecs, "position")->data;
-	testStrComponent* strs = cecs_component(cecs, "string")->data;
-	testUVComponent* uvs = cecs_component(cecs, "uv")->data;
-
-	sys->run();
-
-	for(int i = 0; i < cecs->num_entities; ++i){
-		// if i is in free_entities, should have been ignored
-		if(cecs->free_entities.length != 0){
-			for(int j = 0; j < cecs->free_entities.length; ++i){
-				if(i == cecs->free_entities.data[i])
-					check_ent_untouched(&positions[i],&uvs[i]);
-			}
-		}
-
-		/*
-		 * A component in this ent has been specifically excluded, it
-		 * should have been ignored.
-		 *
-		 * bitwise & will return 0 only if no bit in the ent mask matches
-		 * the exclusion mask
-		*/
-		if((cecs->entities[i].mask & sys->exclusion_mask) != 0) {
-			check_ent_untouched(&positions[i],&uvs[i]);
-		/*
-		 * Entity contains both required components for system to be
-		 * interested, values should have been set.
-		*/
-		} else if ((cecs->entities[i].mask & sys->inclusion_mask) ==
-			  sys->inclusion_mask){
-			/*
-			 * Posiitions were incremented with '+=', but the sys
-			 * has only been run once
-			*/
-			ck_assert_float_eq(positions[i].x, 1.f);
-			ck_assert_float_eq(positions[i].y, 1.5f);
-			ck_assert_float_eq(positions[i].z, 2.f);
-
-			// UVs were set, with '=' so should be 5.f
-			ck_assert_float_eq(uvs[i].u, 5.f);
-			ck_assert_float_eq(uvs[i].v, 5.f);
-		}
-
-		/*
-		 * test sys doesn't care about strings, so they should always
-		 * be NULL
-		*/
-		printf("strs[%d].str is %s\n", i, strs[i].str);
-		ck_assert_ptr_null(strs[i].str);
-	}
-}
-END_TEST
-
-START_TEST(cecs_check_system_rem)
-{
-	cecs_rem_system(cecs, "test-sys");
-	ck_assert_ptr_null(cecs_system(cecs, "test-sys"));
-}
-
 Suite * cecs_suite(void)
 {
 	Suite *s;
 
 	s = suite_create("cecs suite");
 	TCase * tcinit = tcase_create("cecs init");
-	TCase * tccomp = tcase_create("cecs add components");
-	TCase * tcent = tcase_create("cecs add entities");
-	TCase * tcsys = tcase_create("cecs add systems");
 
 	tcase_add_checked_fixture(tcinit, init_setup, init_teardown);
 	tcase_add_test(tcinit, cecs_check_init);
 
-	tcase_add_unchecked_fixture(tcsys, init_setup, init_teardown);
-	tcase_add_unchecked_fixture(tcsys, sys_setup, sys_teardown);
-	tcase_add_test(tcsys, cecs_check_system_func);
-	tcase_add_test(tcsys, cecs_check_add_sys);
-	tcase_add_test(tcsys, cecs_check_system_run);
-	tcase_add_test(tcsys, cecs_check_system_rem);
-
 	suite_add_tcase(s, tcinit);
-	suite_add_tcase(s, tcsys);
 	return s;
 }
 
